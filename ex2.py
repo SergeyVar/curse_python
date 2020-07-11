a2c99c7edbb3d7a18061684b64dc1713a10a25d9
PC <svarganik@gmail.com>
2020-07-11 19:53:36 +0300

    положительные и отрицательные числа

---
+++ ex2.py
@@ -0,0 +1,15 @@
+'''a = int(input())
+b = int(input())
+if a > 0 and b < 0:
+    print('True')
+elif a < 0 and b > 0:
+    print('True')
+elif a > 0 and b > 0 or a < 0 and b < 0:
+    print('False')'''
+
+a = int(input())
+b = int(input())
+if a*b>0:
+    print('False')
+else:
+    print('True')
