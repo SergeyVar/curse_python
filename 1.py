f0d0e239df3e90e2dca7ab8939fb29e8a3648057
PC <svarganik@gmail.com>
2020-07-20 14:40:15 +0300

    Цыклы

---
+++ 1.py
@@ -0,0 +1,25 @@
+n=10
+for j in range(n):
+    print()
+    for i in range(n):
+        print(i,end='  ')
+print()
+for j in range(n):
+    print()
+    for i in range(n):
+        print(j, end='  ')
+print()
+
+n=12
+for i in range(n):
+    print()
+    for i in range(i-1):
+        print(i,end='  ')
+print('\n')
+n=9
+for i in range (1,10):
+    print(i,end='  ')
+
+    for i in range(n):
+          print('0', end='  ')
+    print()
--- черновик.py
+++ черновик.py
@@ -1,38 +1,6 @@
-'''a, b, c, d, e, f = input()
-k1 = int(a) + int(b) + int(c)
-k2 = int(d) + int(e) + int(f)
-if k1 == k2:
-    print("Счастливый")
-else:
-    print("Обычный")'''
+'''for a in range(0,9,2):
+    print(a+a)'''
 
-'''a = 3
-while a < 31:
-    print(a, end=' ')
-    a = a + 3'''
 
-# Имя  и пароль
-'''name=input('Введите ваше имя:\n')
-k=3
-if name == 'Сергей':
-    password = input('Введите пароль:\n')
-    if password=='1234':
-        print('Вход выполнен успешно')
-    else:
-        while k>1:
-            input('Введите пароль\n')
-            k=k-1
-        if k==1:
-            print('Пароль не верен')
-else:
-    while k > 1:
-        input('Введите ваше имя:\n')
-        k = k - 1
-    if k == 1:
-        print('Пользователя с таким именем не существует')'''
-
-name='Sergey'
-age=48
-print('Возраст {0} {1} лет'.format(name,age))
 
 
